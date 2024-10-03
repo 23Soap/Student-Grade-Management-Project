4537ae92fec00400e4805ac559c1cd5fe253375b
Soap <50261583+23Soap@users.noreply.github.com>
2024-10-03 00:42:13 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
+public class Program {
 
-public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("First Grade: ");
+
+        System.out.println("Enter your first Grade: ");
         int grade1 = scanner.nextInt();
-        System.out.println("Second Grade: ");
+        System.out.println("Enter your second Grade: ");
         int grade2 = scanner.nextInt();
-        System.out.println("Third Grade: ");
+        System.out.println("Enter your third Grade: ");
         int grade3 = scanner.nextInt();
-        System.out.println("Fourth Grade: ");
+        System.out.println("Enter your fourth Grade: ");
         int grade4 = scanner.nextInt();
-        System.out.println("Fifth Grade: ");
+        System.out.println("Enter your fifth Grade: ");
         int grade5 = scanner.nextInt();
 
+        System.out.println("search for a grade: ");
+        int searchGrades = scanner.nextInt();
 
-        Students students = new Students(grade1, grade2, grade3, grade4, grade5,scanner);
+        Students students = new Students(grade1, grade2, grade3, grade4, grade5,searchGrades);
+
         students.displayGrades();
         students.calculateAverage();
-//      students.searchGrades();
+        students.searchGrades();
         students.sortGrades();
 
         scanner.close();
-
-
+        
     }
 }
