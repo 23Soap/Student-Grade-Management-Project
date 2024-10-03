cba4a24faafd277cbf87498d843c26dda36002f5
Soap <50261583+23Soap@users.noreply.github.com>
2024-10-03 00:41:29 -0400

    Update Students.java

--- Students.java
+++ Students.java
@@ -1,68 +1,42 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
-public class Students {
+class Students {
+
     private int grade1, grade2, grade3, grade4, grade5;
-    private Scanner scanner;
-    int grades[];
+    int[] allGrades;
+    private int search;
 
-    public Students(int grade1, int grade2, int grade3, int grade4, int grade5, Scanner scanner) {
+    public Students(int grade1, int grade2, int grade3, int grade4, int grade5, int search) {
         this.grade1 = grade1;
         this.grade2 = grade2;
         this.grade3 = grade3;
         this.grade4 = grade4;
         this.grade5 = grade5;
-        this.scanner = scanner;
+        this.search = search;
+        this.allGrades = new int[]{grade1, grade2, grade3, grade4, grade5};
     }
 
     public void displayGrades() {
-        System.out.println("All grades " + grade1 + "," + grade2 + "," + grade3 + "," + grade4 + "," + grade5);
+        int allGrades = grade1 + grade2 + grade3 + grade4 + grade5;
+        System.out.println("All grades: " + grade1 + ", " + grade2 + ", " + grade3 + ", " + grade4 + ", " + grade5);
     }
 
     public void calculateAverage() {
-        int allGrades = grade1 + grade2 + grade3 + grade4 + grade5;
-        double average = allGrades / 5.0;
-        System.out.println("Grades avarage is " + average);
+        double averageGrades = (grade1 + grade2 + grade3 + grade4 + grade5) / 5.0;
+        System.out.println("Average grades: " + averageGrades);
+    }
+
+    public void searchGrades() {
+
+        for (int i = 0; i < allGrades.length; i++) {
+            if (search == allGrades[i]) {
+                System.out.println("Your searching is " + allGrades[i]);
+            }
+        }
     }
 
     public void sortGrades() {
-        int[] grades = {grade1, grade2, grade3, grade4, grade5};
-        System.out.println("Original Grades: ");
-        for (int grade : grades) {
-            System.out.println(grade);
-        }
-        Arrays.sort(grades);
-        System.out.println("Grade is sorted: ");
-        for (int grade : grades) {
-            System.out.println(grade);
-        }
+        Arrays.sort(allGrades);
+        System.out.println("Your sorted grades are" + Arrays.toString(allGrades));
     }
-
-//    public void searchGrades() {
-//
-//        System.out.println("Enter you Grade for search: ");
-//        int gradeSearch = scanner.nextInt();
-//
-//        boolean found = false;
-//
-//        int[] grades = {grade1, grade2, grade3, grade4, grade5};
-//        for (int i = 0; i < grades.length; i++) {
-//            if (grades[i] == gradeSearch) {
-//                found = true;
-//            }
-//            System.out.println(grades[i]);
-//        }for (int grade : grades) {
-//            if (grade == gradeSearch) {
-//                found = true;
-//                System.out.println("Your grade is " + grade);
-//                break;
-//
-//            }
-//        }
-//        if (!found) {
-//            System.out.println("Grade not found.");
-//        }
-//    }
-
-
 }
